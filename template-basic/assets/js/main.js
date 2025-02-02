3dfec11e5c84875b84bec02b2ae86e1becb4e6a1
shintya <ajeng.shintya13@gmail.com>
2025-02-03 01:00:10 +0700

    Update template-basic/index.html

--- template-basic/assets/js/main.js
+++ template-basic/assets/js/main.js
@@ -1,6 +1,27 @@
 (function() {
     "use strict";
 
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50) { // Jika scroll lebih dari 50px
+            $("#navbar").addClass("fixed-top shadow");
+        } else {
+            $("#navbar").removeClass("fixed-top shadow");
+        }
+
+        // Tombol Scroll ke Atas muncul
+        if ($(this).scrollTop() > 200) {
+            $("#scrollTopBtn").fadeIn();
+        } else {
+            $("#scrollTopBtn").fadeOut();
+        }
+    });
+
+    // Aksi klik untuk scroll ke atas
+    $("#scrollTopBtn").click(function() {
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+
     /**
      * Preloader
      */
